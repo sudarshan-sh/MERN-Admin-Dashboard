b8dd86e3e2fa95ae1f5cf479ac05979885fba859
Sudarshan Sharma <mail2sudarshan.sharma@gmail.com>
2022-12-16 01:23:37 +0530

    color setup, MUI & redux installation

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
+import { useSelector } from "react-redux";
+
+function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="app">
+      {/* MUI setup */}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import bodyParser from "body-parser";
-import clientRoutes from "./routes/client";
-import generalRoutes from "./routes/general";
-import managementRoutes from "./routes/management";
-import salesRoutes from "./routes/sales";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 // Configurations
 dotenv.config();
@@ -27,3 +27,15 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedToplogy: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
